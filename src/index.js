e8d14b50c60c86440ba5fee072609adffe6ed425
petermtgi <3sjk0krrr@gmail.com>
2025-06-25 22:13:23 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginPage from './LoginPage'; 
+import LoginPage from './components/LoginPage'; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
