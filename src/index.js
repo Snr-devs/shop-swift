dd8a12c777545dfe61dcc7bb5c597c46fd31b988
petermtgi <3sjk0krrr@gmail.com>
2025-06-26 21:36:40 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginPage from './components/LoginPage'; 
+import LoginPage from './components/LoginPage';
+import CartPage from './components/CartPage';
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // ✅ Add this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPage /> 
+    <BrowserRouter> {/* ✅ Add Router wrapper */}
+      <Routes>       {/* ✅ Handle routes */}
+        <Route path="/" element={<LoginPage />} />       {/* 🏠 Login */}
+        <Route path="/cart" element={<CartPage />} />    {/* 🛒 Cart */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
