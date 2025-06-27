f53b124be08d89c67a4cf46203537ed865171c87
Arnold <Arnold>
2025-06-27 10:52:13 +0300

    added routes and styled all pages

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 
+
 function Account({account}) {    
   return (
     <div>
-        <Navbar />
+        <div className='bars'><Navbar /></div>
 
         <div className='account'>
-       <img src='' alt='user'></img>
+       <img className='image'src="/account-icon-25500.png" alt="user" />
+
        <p>Username:</p>
        <p>Email:</p>
        <p>Phone number:</p>
-       <p>Nationality</p>
+       <p>Nationality: Kenya</p>
        <p></p>
        </div>
        
